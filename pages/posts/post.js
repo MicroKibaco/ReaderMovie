57387a7f035521a6c0b07c0e6a03fce8e96d2cb4
MicroKibaco <yangzy3@asiainfo.com>
2017-03-13 19:54:23 +0800

    JS文件结构与Page页面的生命周期
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -3,17 +3,22 @@ Page({
          data: {},
          onLoad: function (options) {
              // 页面初始化 options为页面跳转所带来的参数
+             console.log("onLoad");
          },
          onReady: function () {
              // 页面渲染完成
+             console.log("onReady");
          },
          onShow: function () {
              // 页面显示
+             console.log("onShow");
          },
          onHide: function () {
              // 页面隐藏
+             console.log("onHide");
          },
          onUnload: function () {
              // 页面关闭
+             console.log("onUnload");
          }
-     });;;;;;;;;
+     });
