e7778bbc38147573f52923579bb7427e4490b0d7
MicroKibaco <yangzy3@asiainfo.com>
2017-03-13 21:10:03 +0800

    数据的绑定
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,9 +1,33 @@
 // pages/posts/post.js
 Page({
          data: {},
+
+         imagePath: "/images/...",
+
+         process: function () {
+
+             var data = "Nov 8 2016";
+
+         },
+         
          onLoad: function (options) {
              // 页面初始化 options为页面跳转所带来的参数
-             console.log("onLoad");
+
+             var post_content1 = {
+                 date: "Sept 18 2016",
+                 title: "اختبار اللغة العربية",
+                 content: "دفّة بالرّغم, وبالرغم وبالتحديد، مع جهة. مسارح يتمكن يتسنّى جهة ان, التي اتفاقية قد كان. فمرّ الوراء ان غير. شيء ما شمال أدوات بالجانب, لكل وشعار اتفاق بـ, بها الإنزال مواقعها في. أثره، الأوربيين تحت و, سقوط بالرغم الإتفاقية عن يبق, في هذا وجهان وإقامة مقاطعة.",
+                 view_num: "112",
+                 collec_num: "96",
+                 img: {
+                     author_img: "/images/avatar/logo_fail.png",
+                     post_img: "/images/post/arabic_pyramid.jpg"
+                 },
+                 image_condition: true
+             };;;
+
+             this.setData(post_content1);
+
          },
          onReady: function () {
              // 页面渲染完成
@@ -21,4 +45,4 @@ Page({
              // 页面关闭
              console.log("onUnload");
          }
-     });
+     });;;
