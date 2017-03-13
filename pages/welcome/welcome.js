85ebbcd98928af1883d87086d2a02785d8ff223c
MicroKibaco <yangzy3@asiainfo.com>
2017-03-13 22:48:00 +0800

    引导页面和新闻列表页面的跳转,比较navigateTo和redirectTo执行的生命周期的方法
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,19 +1,37 @@
 // pages/welcome/weclome.js
 Page({
   data:{},
+
+         onTap: function () {
+             //页面跳转
+             // wx.navigateTo({
+             //   url:"../posts/post"
+             // });
+
+             // 去掉两个页面的平行跳转
+             wx.redirectTo({
+                               url: "../posts/post"
+                           });
+         },
+
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+      console.log("Weclome page is onLoad");
   },
   onReady:function(){
     // 页面渲染完成
+      console.log("Weclome page is onReady");
   },
   onShow:function(){
     // 页面显示
+      console.log("Weclome page is onShow");
   },
   onHide:function(){
     // 页面隐藏
+      console.log("Weclome page is onHide");
   },
   onUnload:function(){
     // 页面关闭
+      console.log("Weclome page is onUnload");
   }
-})
+     });;
