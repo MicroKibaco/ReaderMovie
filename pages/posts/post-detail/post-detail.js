cff39c19abcfdce85a7f315d6c64bea582ae265a
MicroKibaco <yangzy3@asiainfo.com>
2017-03-14 22:30:18 +0800

    缓存Storage的基本用法
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -9,7 +9,30 @@ Page({
              var postId = option.id;
              var postdata = postsData.postsList[postId];
              this.setData({postdata: postdata});
+             //   小程序暂且不支持webview
              //   this.data.postdata = postdata; 在onLoad同步方法里面可以这样用
+
+             //wx.setStorageSync('String',"小木箱");
+
+             //修改缓存
+             wx.setStorageSync('key', {
+                 game: "Android开发",
+                 developer: "杨正友"
+             });
+
+         },
+
+         onCollectionTap: function (event) {
+
+             var game = wx.getStorageSync('key');
+             console.log("game: " + game);
+
+         },
+
+         onShareTap: function (event) {
+
+             wx.removeStorageSyncs('key');
+
          },
 
          onShow: function () {
