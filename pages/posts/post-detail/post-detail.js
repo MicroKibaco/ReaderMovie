0bab53858caad21a98ab9233bc3e00f020b91c94
MicroKibaco <yangzy3@asiainfo.com>
2017-03-15 20:25:34 +0800

    音乐播放的基本实现
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -69,6 +69,16 @@ Page({
 
          },
 
+         onMusicTap: function (event) {
+
+             wx.playBackgroundAudio({
+                                        dataUrl: 'http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38',
+                                        title: "夜夜夜夜-齐秦",
+                                        coverImgUrl: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+                                    });
+
+         },
+
          getpostsCollectedSyc: function () {
                 
              var postsCollected = wx.getStorageSync("posts_collected");
