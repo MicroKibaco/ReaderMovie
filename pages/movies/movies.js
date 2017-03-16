0e5dd63d14e7cf8ad7ce3a2fd46a107f8d437458
MicroKibaco <yangzy3@asiainfo.com>
2017-03-16 21:56:05 +0800

    出现了一个跳转天坑
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -7,4 +7,4 @@ Page({
          onLoad: function () {
 
          }
-     });;;;;;;;;;;;;;
+     });
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,14 +3,16 @@ Page({
   data:{},
 
          onTap: function (event) {
-             // 页面跳转
-             wx.navigateTo({
-                               url: "../posts/post"
-                           });
+             // 页面跳转,为什么redirectTo不出现选项卡?
+             //如果tab第一个页面,无论什么方式跳转都可行
+             wx.switchTab({
+                              url: "../posts/post"
+                          });
 
-             // 去掉两个页面的平行跳转
-             //  wx.redirectTo({
-             //                    url: "../posts/post",
-             //                })
+             //  去掉两个页面的平行跳转
+             //   wx.redirectTo({
+             //                     url: "../posts/post",
+             //                 })
+
          }
      });
