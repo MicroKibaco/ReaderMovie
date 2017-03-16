48421e791a8fb2500f363fe8961b3aec80739729
MicroKibaco <yangzy3@asiainfo.com>
2017-03-16 17:17:26 +0800

    tap选项卡前期准备
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -15,6 +15,10 @@ Page({
 
          },
 
+         // currentTarget和target的区别
+         // currentTarget指的是事件捕获的组件 , target指的是当前点击的组件
+         // currentTarget指的是swiper组件 ,target指的是image组件
+         // catchTap 可以阻止事件的冒泡  bindTap不会阻止事件冒泡
          onPostTap: function (event) {
 
              var postId = event.currentTarget.dataset.postid;
@@ -23,6 +27,18 @@ Page({
                                url: "post-detail/post-detail?id=" + postId
 
                            });
+
+         },
+         onSwiperTap: function (event) {
+
+             var postId = event.target.dataset.postid;
+
+             wx.navigateTo({
+                               url: "post-detail/post-detail?id=" + postId
+
+                           });
+
+
 
          }
      });
