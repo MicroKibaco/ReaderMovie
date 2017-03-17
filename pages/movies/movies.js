cca63ed19adc6da1f1bbc898d73125edd2680ea8
MicroKibaco <yangzy3@asiainfo.com>
2017-03-18 00:01:51 +0800

    电影页面数据绑定
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -4,23 +4,31 @@ Page({
          data: {
              // 小程序总是会读取data对象作数据绑定,这个动作我们称之为动作A
              // 而这个动作A总是在onLoad方法执行完毕之后执行的
+
+             data:{
+
+                 inTheaters : {},
+                 comingSoon : {},
+                 top250 : {}
+
+             }
          },
 
          onLoad: function () {
 
 
-             var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
-             var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
-             var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
+             var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters"+"?start=0&count=3";
+             var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon"+"?start=0&count=3";
+             var top250Url = app.globalData.doubanBase + "/v2/movie/top250"+"?start=0&count=3";
 
-             this.getMovieListData(inTheatersUrl);
-             this.getMovieListData(comingSoonUrl);
-             this.getMovieListData(top250Url);
+             this.getMovieListData(inTheatersUrl,"inTheaters");
+             this.getMovieListData(comingSoonUrl,"comingSoon");
+             this.getMovieListData(top250Url,"top250");
          },
 
-         getMovieListData:function(url){
+         getMovieListData:function(url,settedKey){
 
-                
+             var that = this;
              wx.request({
 
                             url: url,
@@ -33,6 +41,8 @@ Page({
                             success: function (res) {
 
                                 console.log("数据返回成功: " + res);
+                                that.processDouBanData(res.data,settedKey);
+
                             },
 
                             fail: function (error) {
@@ -44,5 +54,46 @@ Page({
 
                         });
 
-         }
+         },
+
+                    processDouBanData : function(moviesDouban,settedKey){
+
+                            var movies = [];
+
+                            for(var idx in moviesDouban.subjects){
+
+                               var subject = moviesDouban.subjects[idx];
+                               var title = subject.title;
+                               if(title.length >= 6){
+
+
+                                   title = title.substring(0,6) + "...";
+
+
+                               }
+
+                               var temp = {
+
+
+                                    title:title,
+                                    average:subject.rating.average,
+                                    coverageUrl:subject.images.large,
+                                    movieId:subject.id,
+
+
+
+
+                               };;;;
+
+
+                               movies.push(temp);
+                            
+
+                            }
+
+                              var readyData = {};
+                              readyData [settedKey] = movies;
+                              this.setData(readyData);
+                        
+                    }
      });
