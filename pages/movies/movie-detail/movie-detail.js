2188b682983d4389fe3014138cd06d06a2e685df
MicroKibaco <yangzy3@asiainfo.com>
2017-03-20 19:22:42 +0800

    编写电影详情页面(上)
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,9 +1,21 @@
+var util = require('../../../utils/util.js');
+var app = getApp();
+
 Page({
   data:{},
   onLoad:function(options){
 
       var movieId = options.id;
-      console.log("movieId: " + movieId);
+      var url = app.globalData.doubanBase + "/v2/movie/subject/" + movieId;
+      util.http(url,this.processDoubanData);
 
+      
   },
+
+  processDoubanData:function(data){
+
+      console.log(data);
+
+  }
+  
 });
