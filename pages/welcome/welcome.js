7a9426281c1d0210127f3e941ade841b620c0b60
MicroKibaco <yangzy3@asiainfo.com>
2017-03-21 20:20:00 +0800

    小楼昨夜又秋分
    Signed-off-by: MicroKibaco <yangzy3@asiainfo.com>

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -5,6 +5,7 @@ Page({
          onTap: function (event) {
              // 页面跳转,为什么redirectTo不出现选项卡?
              //如果tab第一个页面,无论什么方式跳转都可行
+             //奇淫技巧:可以在相对路径前，加少于5个"../",都可以跳转成功
              wx.switchTab({
                               url: "../posts/post"
                           });
